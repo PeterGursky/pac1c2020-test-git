0b1156c799250aa2a5d9f01a95fd4fdd8fef4612
PeterGursky <peter.gursky@upjs.sk>
2020-09-23 11:53:37 +0200

    zmena prveho pokial menil druhy

--- src/Prva.java
+++ src/Prva.java
@@ -4,6 +4,7 @@ public class Prva {
 	public static void main(String[] args) {
 		System.out.println("Peter Gursky");
 		System.out.println("Zmena pre druheho");
+		System.out.println("Zmena v prvej pokial druhy uz komitol druhu");
 	}
 
 }
